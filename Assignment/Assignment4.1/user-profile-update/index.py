6723330e9259ed58b0d3ebdcd0e12aec439b1a6f
jyoti <jyoti1.satya@gmail.com>
2024-08-05 18:13:34 +0530

    added test spec fror 4.1

--- Assignment/Assignment4.1/user-profile-update/index.py
+++ Assignment/Assignment4.1/user-profile-update/index.py
@@ -4,6 +4,3 @@ def user_update(input_dict, main_key, update_key, update_value):
             if key == main_key:
                 input_dict[key].update({update_key:update_value})
     return input_dict
-
-# result = user_update(user_profile,"address", "city", "Bangalore")  
-# print(json.dumps(result , indent=3))          
--- Assignment/Assignment4.1/user-profile-update/spec.py
+++ Assignment/Assignment4.1/user-profile-update/spec.py
@@ -1,4 +1,4 @@
-
+import json
 from index import user_update 
 from data import user_profile
 import unittest
@@ -6,21 +6,9 @@ class TestFindMeaning(unittest.TestCase):
 
     def test_old_city_value_not_exists(self):
         old_city_value = 'bengaluru'
-        # result = user_update(user_profile,"address", "city", "Bangalore")
-        result  = {
-        "address" : {
-            "city": "Bangalore",
-            "pincode" : 560100,
-            "state" : "karnataka"
-        },
-        "courses" : [ "python", "javascript", "java"],
-        "name" : "jyoti",
-        "age" : 56,
-        "email" : "js@example.com"
-   }
-        self.assertNotEqual(old_city_value, result['city'])
-
-        
+        result = user_update(user_profile,"address", "city", "Bangalore")
+        new_city = result['address']['city']
+        self.assertNotEqual(old_city_value, new_city , f"Expected {new_city} but got {old_city_value}")
 
 if __name__ == '__main__':
     unittest.main()
